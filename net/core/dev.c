5692e84e3307daff97d6f398112ba6f6d23e2622
Alexander Duyck <alexander.h.duyck@intel.com>
2011-06-08 12:35:08 +0000

    v2 ethtool: remove support for ETHTOOL_GRXNTUPLE
    This change is meant to remove all support for displaying an ntuple as
    strings via ETHTOOL_GRXNTUPLE.  The reason for this change is due to the
    fact that multiple issues have been found including:
     - Multiple buffer overruns for strings being displayed.
     - Incorrect filters displayed, cleared filters with ring of -2 are displayed
     - Setting get_rx_ntuple displays no rules if defined.
     - Endianess wrong on displayed values.
     - Hard limit of 1024 filters makes display functionality extremely limited
    
    The only driver that had supported this interface was ixgbe.  Since it no
    longer uses the interface and due to the issues mentioned above I am
    submitting this patch to remove it.
    
    v2:
    Updated based on comments from Ben Hutchings
     - Left ETH_SS_NTUPLE_FILTERS in code but commented on it being deprecated
     - Removed ethtool_rx_ntuple_list and ethtool_rx_ntuple_flow_spec_container
     - Left ETHTOOL_GRXNTUPLE but commented it as deprecated
    
    Also cleaned up set_rx_ntuple since there is no flow spec container to
    maintain we can drop all the code for the alloc and free of it and just
    return ops->set_rx_ntuple().
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Acked-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5900,8 +5900,6 @@ struct net_device *alloc_netdev_mqs(int sizeof_priv, const char *name,
 	dev->gso_max_size = GSO_MAX_SIZE;
 	dev->gso_max_segs = GSO_MAX_SEGS;
 
-	INIT_LIST_HEAD(&dev->ethtool_ntuple_list.list);
-	dev->ethtool_ntuple_list.count = 0;
 	INIT_LIST_HEAD(&dev->napi_list);
 	INIT_LIST_HEAD(&dev->unreg_list);
 	INIT_LIST_HEAD(&dev->link_watch_list);
@@ -5964,9 +5962,6 @@ void free_netdev(struct net_device *dev)
 
 	/* Flush device addresses */
 	dev_addr_flush(dev);
-
-	/* Clear ethtool n-tuple list */
-	ethtool_ntuple_flush(dev);
 
 	list_for_each_entry_safe(p, n, &dev->napi_list, dev_list)
 		netif_napi_del(p);
