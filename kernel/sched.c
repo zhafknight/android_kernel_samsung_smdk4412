5c1826ba90ca691f5b893d40bb14ad1b11435b7a
Thomas Gleixner <tglx@linutronix.de>
2011-09-15 15:32:06 +0200

    sched: Fix idle_cpu()
    On -rt we observed hackbench waking all 400 tasks to a single cpu.
    This is because of select_idle_sibling()'s interaction with the new
    ipi based wakeup scheme.
    
    The existing idle_cpu() test only checks to see if the current task on
    that cpu is the idle task, it does not take already queued tasks into
    account, nor does it take queued to be woken tasks into account.
    
    If the remote wakeup IPIs come hard enough, there won't be time to
    schedule away from the idle task, and would thus keep thinking the cpu
    was in fact idle, regardless of the fact that there were already
    several hundred tasks runnable.
    
    We couldn't reproduce on mainline, but there's no reason it couldn't
    happen.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-3o30p18b2paswpc9ohy2gltp@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5108,7 +5108,20 @@ int task_prio(const struct task_struct *p)
  */
 int idle_cpu(int cpu)
 {
-	return cpu_curr(cpu) == cpu_rq(cpu)->idle;
+	struct rq *rq = cpu_rq(cpu);
+
+	if (rq->curr != rq->idle)
+		return 0;
+
+	if (rq->nr_running)
+		return 0;
+
+#ifdef CONFIG_SMP
+	if (!llist_empty(&rq->wake_list))
+		return 0;
+#endif
+
+	return 1;
 }
 
 /**
