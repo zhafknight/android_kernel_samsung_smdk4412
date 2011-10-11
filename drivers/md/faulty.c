e3a4278bbd26ba9d3de768e7e122342b2bd567e4
NeilBrown <neilb@suse.de>
2011-10-11 16:49:58 +1100

    md: rename "mdk_personality" to "md_personality"
    "mdk" doesn't mean anything any more.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/faulty.c
+++ drivers/md/faulty.c
@@ -335,7 +335,7 @@ static int stop(struct mddev *mddev)
 	return 0;
 }
 
-static struct mdk_personality faulty_personality =
+static struct md_personality faulty_personality =
 {
 	.name		= "faulty",
 	.level		= LEVEL_FAULTY,
