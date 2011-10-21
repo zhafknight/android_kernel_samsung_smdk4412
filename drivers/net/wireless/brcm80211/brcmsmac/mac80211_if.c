31501970909f28be75cd7975998f1ac7bd67d5c3
Roland Vossen <rvossen@broadcom.com>
2011-10-21 16:16:24 +0200

    brcm80211: smac: removed down-on-rf-kill functionality
    Softmac would bring its interface down on an RF kill switch condition,
    without Mac80211 intervention. Because Mac80211 should be the only party
    initiating interfaces going up and down, this functionality has been
    removed.
    
    Reviewed-by: Alwin Beukers <alwin@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmsmac/mac80211_if.c
+++ drivers/net/wireless/brcm80211/brcmsmac/mac80211_if.c
@@ -1079,8 +1079,6 @@ static struct brcms_info *brcms_attach(u16 vendor, u16 device,
 
 	wl->pub->ieee_hw = hw;
 
-	brcms_c_set_radio_mon(wl->wlc);
-
 	/* register our interrupt handler */
 	if (request_irq(irq, brcms_isr, IRQF_SHARED, KBUILD_MODNAME, wl)) {
 		wiphy_err(wl->wiphy, "wl%d: request_irq() failed\n", unit);
