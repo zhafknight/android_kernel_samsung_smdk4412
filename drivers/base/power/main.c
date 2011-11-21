61568b159394a05dde6aa1126aef91a04e1d4f34
Rafael J. Wysocki <rjw@sisk.pl>
2011-11-21 23:33:55 +0100

    PM / Sleep: Simplify device_suspend_noirq()
    Remove a few if () and return statements in device_suspend_noirq()
    that aren't really necessary.
    
    Change-Id: Ie6b527f3f678bbd1a2156b9181cb662029e0927f
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Reviewed-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -812,42 +812,20 @@ static pm_message_t resume_event(pm_message_t sleep_state)
 static int device_suspend_noirq(struct device *dev, pm_message_t state)
 {
 	int error = 0;
-	struct timer_list timer;
-	struct dpm_drv_wd_data data;
-
-	data.dev = dev;
-	data.tsk = get_current();
-	init_timer_on_stack(&timer);
-	timer.expires = jiffies + HZ * 12;
-	timer.function = dpm_drv_timeout;
-	timer.data = (unsigned long)&data;
-	add_timer(&timer);
 
 	if (dev->pm_domain) {
 		pm_dev_dbg(dev, state, "LATE power domain ");
 		error = pm_noirq_op(dev, &dev->pm_domain->ops, state);
-		if (error)
-			goto exit;
 	} else if (dev->type && dev->type->pm) {
 		pm_dev_dbg(dev, state, "LATE type ");
 		error = pm_noirq_op(dev, dev->type->pm, state);
-		if (error)
-			goto exit;
 	} else if (dev->class && dev->class->pm) {
 		pm_dev_dbg(dev, state, "LATE class ");
 		error = pm_noirq_op(dev, dev->class->pm, state);
-		if (error)
-			goto exit;
 	} else if (dev->bus && dev->bus->pm) {
 		pm_dev_dbg(dev, state, "LATE ");
 		error = pm_noirq_op(dev, dev->bus->pm, state);
-		if (error)
-			goto exit;
 	}
-
-exit:
-	del_timer_sync(&timer);
-	destroy_timer_on_stack(&timer);
 
 	return error;
 }
