0e348aa31e36a4875c9eae841ff6acd968569097
NeilBrown <neilb@suse.de>
2012-10-11 13:50:12 +1100

    md/raid5: protect debug message against NULL derefernce.
    The pr_debug in add_stripe_bio could race with something
    changing *bip, so it is best to hold the lock until
    after the pr_debug.
    
    Change-Id: I86c271a83bc7c735b9600fa38132701807c7d62e
    Reported-by: "Jianpeng Ma" <majianpeng@gmail.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -2406,6 +2406,7 @@ static int add_stripe_bio(struct stripe_head *sh, struct bio *bi, int dd_idx, in
 	pr_debug("added bi b#%llu to stripe s#%llu, disk %d.\n",
 		(unsigned long long)(*bip)->bi_sector,
 		(unsigned long long)sh->sector, dd_idx);
+	spin_unlock_irq(&sh->stripe_lock);
 
 	if (conf->mddev->bitmap && firstwrite) {
 		bitmap_startwrite(conf->mddev->bitmap, sh->sector,
