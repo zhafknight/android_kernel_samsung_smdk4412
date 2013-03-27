a26a74434af1d769f22ff6583b7eb37f0a1e5ece
Hong zhi guo <honkiko@gmail.com>
2013-03-27 06:49:35 +0000

    selinux: replace obsolete NLMSG_* with type safe nlmsg_*
    Signed-off-by: Hong Zhiguo <honkiko@gmail.com>
    Acked-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I3ef7572b0fa0199710a1e9137c88c4b03faaea0b

--- security/selinux/netlink.c
+++ security/selinux/netlink.c
@@ -15,7 +15,6 @@
 #include <linux/stddef.h>
 #include <linux/kernel.h>
 #include <linux/skbuff.h>
-#include <linux/netlink.h>
 #include <linux/selinux_netlink.h>
 #include <net/net_namespace.h>
 #include <net/netlink.h>
@@ -74,7 +73,7 @@ static void selnl_notify(int msgtype, void *data)
 
 	len = selnl_msglen(msgtype);
 
-	skb = alloc_skb(NLMSG_SPACE(len), GFP_USER);
+	skb = nlmsg_new(len, GFP_USER);
 	if (!skb)
 		goto oom;
 
