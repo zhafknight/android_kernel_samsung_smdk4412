93a8b1721f70c4c0f60d8c84c88594b6386862cd
Johannes Berg <johannes.berg@intel.com>
2013-11-19 15:19:38 +0100

    genetlink: pass family to functions using groups
    This doesn't really change anything, but prepares for the
    next patch that will change the APIs to pass the group ID
    within the family, rather than the global group ID.
    
    Change-Id: I58cb57231e148068e7f92310d280d72e6be1f984
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/thermal/thermal_sys.c
+++ drivers/thermal/thermal_sys.c
@@ -1491,7 +1491,8 @@ int thermal_generate_netlink_event(u32 orig, enum events event)
 		return result;
 	}
 
-	result = genlmsg_multicast(skb, 0, thermal_event_mcgrp.id, GFP_ATOMIC);
+	result = genlmsg_multicast(&thermal_event_genl_family, skb, 0,
+				   thermal_event_mcgrp.id, GFP_ATOMIC);
 	if (result)
 		pr_info("failed to send netlink event:%d\n", result);
 
