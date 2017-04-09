2f4f85c8798221d93dac86207fbf9678c2265dac
Dominggoes Isakh <drjisakh@gmail.com>
2017-04-09 19:00:19 +0200

    smdk4210: PegasusQ: Set FREQ_FOR_RESPONSIVENESS down to 200Mhz
    We have PegasusQ Boost to boost the CPU frequency.
    
    Change-Id: Ie32ce3bd5f2fd45b306d1feeaff44eb9228aa1a0

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -165,7 +165,12 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_START_DELAY				(0)
 
 #define UP_THRESHOLD_AT_MIN_FREQ		(40)
+
+#ifdef CONFIG_CPU_EXYNOS4210
+#define FREQ_FOR_RESPONSIVENESS			(200000)
+#else
 #define FREQ_FOR_RESPONSIVENESS			(500000)
+#endif
 
 #define HOTPLUG_DOWN_INDEX			(0)
 #define HOTPLUG_UP_INDEX			(1)
@@ -181,7 +186,7 @@ static int hotplug_freq[4][2] = {
 	{200000, 500000},
 	{200000, 0}
 };
-#elif CONFIG_MACH_SMDK4210
+#elif CONFIG_CPU_EXYNOS4210
 static int hotplug_rq[2][2] = {
 	{0, 100}, {100, 0}
 };
