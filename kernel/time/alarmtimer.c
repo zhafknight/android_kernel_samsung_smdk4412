8072608feec0542d30438fa7acc14c7899629c06
Dominggoes Isakh <drjisakh@gmail.com>
2017-11-27 10:34:08 +0100

    Revert "alarmtimer: Don't call rtc_timer_init() when CONFIG_RTC_CLASS=n"
    This reverts commit ad68800e7d918fb280247b5e718763179a022ca2.

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -46,9 +46,10 @@ static struct alarm_base {
 static ktime_t freezer_delta;
 static DEFINE_SPINLOCK(freezer_delta_lock);
 
+static struct rtc_timer		rtctimer;
+
 #ifdef CONFIG_RTC_CLASS
 /* rtc timer and device for setting alarm wakeups at suspend */
-static struct rtc_timer		rtctimer;
 static struct rtc_device	*rtcdev;
 static DEFINE_SPINLOCK(rtcdev_lock);
 
@@ -96,11 +97,6 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 	return 0;
 }
 
-static inline void alarmtimer_rtc_timer_init(void)
-{
-	rtc_timer_init(&rtctimer, NULL, NULL);
-}
-
 static struct class_interface alarmtimer_rtc_interface = {
 	.add_dev = &alarmtimer_rtc_add_device,
 };
@@ -122,7 +118,6 @@ static inline struct rtc_device *alarmtimer_get_rtcdev(void)
 #define rtcdev (NULL)
 static inline int alarmtimer_rtc_interface_setup(void) { return 0; }
 static inline void alarmtimer_rtc_interface_remove(void) { }
-static inline void alarmtimer_rtc_timer_init(void) { }
 #endif
 
 /**
@@ -789,7 +784,7 @@ static int __init alarmtimer_init(void)
 		.nsleep		= alarm_timer_nsleep,
 	};
 
-	alarmtimer_rtc_timer_init();
+	rtc_timer_init(&rtctimer, NULL, NULL);
 
 	posix_timers_register_clock(CLOCK_REALTIME_ALARM, &alarm_clock);
 	posix_timers_register_clock(CLOCK_BOOTTIME_ALARM, &alarm_clock);
