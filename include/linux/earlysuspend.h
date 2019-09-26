983b33f59c4b9d419056cbd4780876b249d0f2f3
Shilin Victor <chrono.monochrome@gmail.com>
2019-09-26 16:55:43 +0300

    Revert "HACK: gpio-keys: force wake up device and do not report input event"
    This reverts commit 07f8768f014647a72fc7bfd684d1e463f5dd9032.
    
    Change-Id: If760bda1a71522052f14aec0c491fb228726c090

--- include/linux/earlysuspend.h
+++ include/linux/earlysuspend.h
@@ -16,7 +16,6 @@
 #ifndef _LINUX_EARLYSUSPEND_H
 #define _LINUX_EARLYSUSPEND_H
 
-#include <linux/suspend.h>
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/list.h>
 #endif
@@ -53,6 +52,4 @@ void unregister_early_suspend(struct early_suspend *handler);
 #define unregister_early_suspend(handler) do { } while (0)
 #endif
 
-void request_suspend_state(suspend_state_t new_state);
-suspend_state_t get_suspend_state(void);
 #endif
