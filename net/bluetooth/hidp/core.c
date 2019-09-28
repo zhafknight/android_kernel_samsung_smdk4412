4852622596d6aaf4dd9ba0d63bee2396f83a3dba
Shilin Victor <chrono.monochrome@gmail.com>
2019-09-28 18:02:29 +0300

    Revert "Merge branch 'for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/jikos/hid"
    This reverts commit 8391b711e5a109829ab06567823371d606874ad6, reversing
    changes made to 088f031d7f2e85dc2452dee38a0ab41d47b6a35e.

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -945,13 +945,6 @@ static int hidp_setup_hid(struct hidp_session *session,
 	hid->hid_get_raw_report = hidp_get_raw_report;
 	hid->hid_output_raw_report = hidp_output_raw_report;
 
-	/* True if device is blacklisted in drivers/hid/hid-core.c */
-	if (hid_ignore(hid)) {
-		hid_destroy_device(session->hid);
-		session->hid = NULL;
-		return -ENODEV;
-	}
-
 	return 0;
 
 fault:
@@ -1024,7 +1017,7 @@ int hidp_add_connection(struct hidp_connadd_req *req, struct socket *ctrl_sock, 
 
 	if (req->rd_size > 0) {
 		err = hidp_setup_hid(session, req);
-		if (err && err != -ENODEV)
+		if (err)
 			goto purge;
 	}
 
