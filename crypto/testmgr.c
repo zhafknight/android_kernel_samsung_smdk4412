11b951323ba7ac958fe26cd2495eb41cdaf99774
Shilin Victor <chrono.monochrome@gmail.com>
2019-09-29 18:28:36 +0300

    Revert "crypto: testmgr - add empty test vectors for null ciphers"
    This reverts commit ee1933dac461167c4673b054b5ab0199c616abb5.
    
    Change-Id: I9df9fc649e8cc4ad5a9ca9cb9d2359bac2219b02

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -1913,9 +1913,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
-		.alg = "compress_null",
-		.test = alg_test_null,
-	}, {
 		.alg = "crc32c",
 		.test = alg_test_crc32c,
 		.fips_allowed = 1,
@@ -2130,9 +2127,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
-		.alg = "digest_null",
-		.test = alg_test_null,
-	}, {
 		.alg = "ecb(__aes-aesni)",
 		.test = alg_test_null,
 		.fips_allowed = 1,
@@ -2242,9 +2236,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 				}
 			}
 		}
-	}, {
-		.alg = "ecb(cipher_null)",
-		.test = alg_test_null,
 	}, {
 		.alg = "ecb(des)",
 		.test = alg_test_skcipher,
