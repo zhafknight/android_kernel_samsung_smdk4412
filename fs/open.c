d543e8b06ee5b64fdcbaf254578dc9b6bd0c603d
Shilin Victor <chrono.monochrome@gmail.com>
2019-09-30 17:50:27 +0300

    fs: open: reset to 790eac5640ab~
    Change-Id: I09398244be48e62ad9bc9548f6267dd790fc1fbd

--- fs/open.c
+++ fs/open.c
@@ -32,9 +32,6 @@
 #include <linux/dnotify.h>
 #include <linux/compat.h>
 
-#define CREATE_TRACE_POINTS
-#include <trace/events/fs.h>
-
 #include "internal.h"
 
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
@@ -949,7 +946,6 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 			} else {
 				fsnotify_open(f);
 				fd_install(fd, f);
-				trace_do_sys_open(tmp, flags, mode);
 			}
 		}
 		putname(tmp);
