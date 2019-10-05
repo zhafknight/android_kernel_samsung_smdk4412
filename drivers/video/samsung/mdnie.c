ac62ed7a2fc71eeeb97ae9fdc7185b8c17337828
Shilin Victor <chrono.monochrome@gmail.com>
2019-10-05 20:26:36 +0300

    Revert "mdnie: update from lineage-16.0"
    This reverts commit ef75f3e6d493a0f54e28c65a6f215c85966fc8b2.

--- drivers/video/samsung/mdnie.c
+++ drivers/video/samsung/mdnie.c
@@ -59,8 +59,6 @@
 #include "mdnie_table_p4note.h"
 #elif defined(CONFIG_FB_S5P_S6D6AA1) && defined(CONFIG_MACH_GD2)
 #include "mdnie_table_gd2.h"
-#elif defined(CONFIG_FB_S5P_S6D6AA1) && defined(CONFIG_MACH_SF2)
-#include "mdnie_table_sf2.h"
 #elif defined(CONFIG_FB_S5P_S6D6AA1)
 #include "mdnie_table_gc1.h"
 #elif defined(CONFIG_FB_S5P_LMS501XX)
@@ -1007,6 +1005,7 @@ static const struct attribute_group *mdnie_groups[] = {
 
 #ifdef CONFIG_PM
 #if defined(CONFIG_FB)
+#if defined(CONFIG_FB_MDNIE_PWM)
 static void mdnie_fb_suspend(struct mdnie_info *mdnie)
 {
 	if (mdnie->fb_suspended)
@@ -1014,9 +1013,11 @@ static void mdnie_fb_suspend(struct mdnie_info *mdnie)
 
 	mdnie->fb_suspended = true;
 
-	dev_info(mdnie->dev, "+%s\n", __func__);
+	struct lcd_platform_data *pd = mdnie->lcd_pd;
 
+	dev_info(mdnie->dev, "+%s\n", __func__);
 	printk("%s: scenario:%d accessibility:%d", __func__, mdnie->scenario, mdnie->accessibility);
+
 #if defined(CONFIG_FB_MDNIE_PWM)
 	struct lcd_platform_data *pd = mdnie->lcd_pd;
 
@@ -1027,7 +1028,6 @@ static void mdnie_fb_suspend(struct mdnie_info *mdnie)
 
 	if (pd && pd->power_on)
 		pd->power_on(NULL, 0);
-
 #endif
 	dev_info(mdnie->dev, "-%s\n", __func__);
 
@@ -1040,6 +1040,9 @@ static void mdnie_fb_resume(struct mdnie_info *mdnie)
 		return;
 
 	mdnie->fb_suspended = false;
+#if defined(CONFIG_FB_MDNIE_PWM)
+	struct lcd_platform_data *pd = mdnie->lcd_pd;
+#endif
 
 	dev_info(mdnie->dev, "+%s\n", __func__);
 
@@ -1058,6 +1061,7 @@ static void mdnie_fb_resume(struct mdnie_info *mdnie)
 
 	mdnie->bd_enable = TRUE;
 #endif
+
 	mdnie_update(mdnie);
 
 	dev_info(mdnie->dev, "-%s\n", __func__);
@@ -1084,7 +1088,9 @@ static int fb_notifier_callback(struct notifier_block *self,
 					break;
 				default:
 				case FB_BLANK_POWERDOWN:
+#if defined(CONFIG_FB_MDNIE_PWM)
 					mdnie_fb_suspend(mdnie);
+#endif
 					break;
 			}
 		}
