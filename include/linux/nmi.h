36abcf8e4f8397e01dbf759cb410cdb156122cd9
Shilin Victor <chrono.monochrome@gmail.com>
2019-10-11 19:37:00 +0300

    Revert "nmi: provide the option to issue an NMI back trace to every cpu but current"
    This reverts commit f3aca3d09525f87731ba6b892c9b010570bc54b4.
    
    Change-Id: I1ba0231e38690cab4886a94643398647f294e960

--- include/linux/nmi.h
+++ include/linux/nmi.h
@@ -32,21 +32,12 @@ static inline void touch_nmi_watchdog(void)
 #ifdef arch_trigger_all_cpu_backtrace
 static inline bool trigger_all_cpu_backtrace(void)
 {
-	arch_trigger_all_cpu_backtrace(true);
+	arch_trigger_all_cpu_backtrace();
 
-	return true;
-}
-static inline bool trigger_allbutself_cpu_backtrace(void)
-{
-	arch_trigger_all_cpu_backtrace(false);
 	return true;
 }
 #else
 static inline bool trigger_all_cpu_backtrace(void)
-{
-	return false;
-}
-static inline bool trigger_allbutself_cpu_backtrace(void)
 {
 	return false;
 }
