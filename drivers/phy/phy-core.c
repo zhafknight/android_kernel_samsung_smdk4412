fd10880ebca53a5058598746ba356e350926c0e3
Shilin Victor <chrono.monochrome@gmail.com>
2019-10-13 00:34:50 +0300

    Revert "phy: core: Support regulator supply for PHY power"
    This reverts commit 3be88125d85df587085b0be0a5c0e9953eb5ed6b.
    
    Change-Id: I38c66f59a54e54a78aad83a50b082e59313cdbfd

--- drivers/phy/phy-core.c
+++ drivers/phy/phy-core.c
@@ -21,7 +21,6 @@
 #include <linux/phy/phy.h>
 #include <linux/idr.h>
 #include <linux/pm_runtime.h>
-#include <linux/regulator/consumer.h>
 
 static struct class *phy_class;
 static DEFINE_MUTEX(phy_provider_mutex);
@@ -232,12 +231,6 @@ int phy_power_on(struct phy *phy)
 	if (!phy)
 		return 0;
 
-	if (phy->pwr) {
-		ret = regulator_enable(phy->pwr);
-		if (ret)
-			return ret;
-	}
-
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
@@ -259,8 +252,6 @@ int phy_power_on(struct phy *phy)
 out:
 	mutex_unlock(&phy->mutex);
 	phy_pm_runtime_put_sync(phy);
-	if (phy->pwr)
-		regulator_disable(phy->pwr);
 
 	return ret;
 }
@@ -285,9 +276,6 @@ int phy_power_off(struct phy *phy)
 	--phy->power_count;
 	mutex_unlock(&phy->mutex);
 	phy_pm_runtime_put(phy);
-
-	if (phy->pwr)
-		regulator_disable(phy->pwr);
 
 	return 0;
 }
@@ -614,16 +602,6 @@ struct phy *phy_create(struct device *dev, struct device_node *node,
 		goto free_phy;
 	}
 
-	/* phy-supply */
-	phy->pwr = regulator_get_optional(dev, "phy");
-	if (IS_ERR(phy->pwr)) {
-		if (PTR_ERR(phy->pwr) == -EPROBE_DEFER) {
-			ret = -EPROBE_DEFER;
-			goto free_ida;
-		}
-		phy->pwr = NULL;
-	}
-
 	device_initialize(&phy->dev);
 	mutex_init(&phy->mutex);
 
@@ -652,9 +630,6 @@ struct phy *phy_create(struct device *dev, struct device_node *node,
 put_dev:
 	put_device(&phy->dev);  /* calls phy_release() which frees resources */
 	return ERR_PTR(ret);
-
-free_ida:
-	ida_simple_remove(&phy_ida, phy->id);
 
 free_phy:
 	kfree(phy);
@@ -841,7 +816,6 @@ static void phy_release(struct device *dev)
 
 	phy = to_phy(dev);
 	dev_vdbg(dev, "releasing '%s'\n", dev_name(dev));
-	regulator_put(phy->pwr);
 	ida_simple_remove(&phy_ida, phy->id);
 	kfree(phy);
 }
