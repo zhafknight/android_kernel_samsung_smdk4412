34d80bd4debfd5b8ab32db5cccaf35477d247e5f
Shilin Victor <chrono.monochrome@gmail.com>
2019-10-13 03:33:02 +0300

    arm: asm/gpio: fix ARCH_NR_GPIOS macro
    Change-Id: I94d75bc20b8366d088ab352dcf2ab250828e311b

--- arch/arm/include/asm/gpio.h
+++ arch/arm/include/asm/gpio.h
@@ -1,9 +1,8 @@
 #ifndef _ARCH_ARM_GPIO_H
 #define _ARCH_ARM_GPIO_H
 
-/* not all ARM platforms necessarily support this API ... */
-#ifdef CONFIG_NEED_MACH_GPIO_H
-#include <mach/gpio.h>
+#if CONFIG_ARCH_NR_GPIO > 0
+#define ARCH_NR_GPIOS CONFIG_ARCH_NR_GPIO
 #endif
 
 /* Note: this may rely upon the value of ARCH_NR_GPIOS set in mach/gpio.h */
