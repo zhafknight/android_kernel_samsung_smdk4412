2ea7a44f626761c90ae3bc4de71abfdfa684fe40
Shilin Victor <chrono.monochrome@gmail.com>
2019-10-16 18:22:29 +0300

    power: porting on 3.16
    Change-Id: I32f4fa97ec994134a21211f89bb562b13b24ec0b

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -380,10 +380,8 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 	state = decode_state(buf, n);
 	if (state < PM_SUSPEND_MAX) {
 #ifdef CONFIG_EARLYSUSPEND
-		if (state == PM_SUSPEND_ON || valid_state(state)) {
-			error = 0;
-			request_suspend_state(state);
-		}
+		error = 0;
+		request_suspend_state(state);
 #else
 		error = pm_suspend(state);
 #endif
