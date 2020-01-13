ca0fee67701037d2e57bd0d59a2945ba0fa41588
Dominggoes Isakh <drjisakh@gmail.com>
2020-01-13 22:04:57 +0100

    bcm4334: Switch from wakelocks to wakeup sources
    Change-Id: I81700e75f02c0b63c0905ced7e0e382f74eca34d

--- arch/arm/mach-exynos/board-bluetooth-bcm4334.c
+++ arch/arm/mach-exynos/board-bluetooth-bcm4334.c
@@ -50,7 +50,7 @@ struct bcm_bt_lpm {
 
 	struct uart_port *uport;
 
-	struct wake_lock wake_lock;
+	struct wakeup_source wake_lock;
 	char wake_lock_name[100];
 } bt_lpm;
 
@@ -131,7 +131,7 @@ static void set_wake_locked(int wake)
 {
 
 	if (!wake)
-		wake_unlock(&bt_lpm.wake_lock);
+		__pm_relax(&bt_lpm.wake_lock);
 
 	gpio_set_value(GPIO_BT_WAKE, wake);
 }
@@ -169,13 +169,13 @@ static void update_host_wake_locked(int host_wake)
 
 	bt_is_running = 1;
 	if (host_wake) {
-		wake_lock(&bt_lpm.wake_lock);
+		__pm_stay_awake(&bt_lpm.wake_lock);
 	} else  {
 		/* Take a timed wakelock, so that upper layers can take it.
 		 * The chipset deasserts the hostwake lock, when there is no
 		 * more data to send.
 		 */
-		wake_lock_timeout(&bt_lpm.wake_lock, HZ/2);
+		__pm_wakeup_event(&bt_lpm.wake_lock, HZ/2);
 	}
 }
 
@@ -214,7 +214,7 @@ static int bcm_bt_lpm_init(struct platform_device *pdev)
 
 	snprintf(bt_lpm.wake_lock_name, sizeof(bt_lpm.wake_lock_name),
 			"BTLowPower");
-	wake_lock_init(&bt_lpm.wake_lock, WAKE_LOCK_SUSPEND,
+	wakeup_source_init(&bt_lpm.wake_lock,
 			 bt_lpm.wake_lock_name);
 
 	irq = IRQ_BT_HOST_WAKE;
@@ -312,7 +312,7 @@ static int bcm4334_bluetooth_remove(struct platform_device *pdev)
 	gpio_free(GPIO_BT_WAKE);
 	gpio_free(GPIO_BT_HOST_WAKE);
 
-	wake_lock_destroy(&bt_lpm.wake_lock);
+	wakeup_source_trash(&bt_lpm.wake_lock);
 	return 0;
 }
 
