e547ae5c649b9f35bff7ed9fd0db47691df85a71
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-01-12 13:34:44 +0100

    Set CPU frequenci to 1600MHz as default for n80xx (P4NOTE).

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -726,6 +726,16 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	ret = cpufreq_frequency_table_cpuinfo(policy, exynos_info->freq_table);
+
+	/* Set default startup frq. */
+#ifdef CONFIG_MACH_P4NOTE
+    	policy->max = 1600000;
+    	policy->min = 100000;
+#else
+    	policy->max = 1400000;
+    	policy->min = 100000;
+#endif
+
 	if (ret)
 		return ret;
 
