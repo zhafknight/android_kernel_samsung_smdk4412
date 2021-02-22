pascua28/Elite-Boeffla-Kernel

cpufreq: Dynamic voltage stepping

arch/arm/mach-exynos/include/mach/cpufreq.h
@@ -14,6 +14,8 @@
  * This should be same with cpufreq_frequency_table
 */
 
+#define CPUFREQ_LEVEL_END	(L19 + 1)
+
 enum cpufreq_level_index {
 	L0, L1, L2, L3, L4,
 	L5, L6, L7, L8, L9,

zhafknight/android_kernel_samsung_smdk4412

Change-Id: Ia68a91a380b327590d203c5f74116015088e7ee6

arch/arm/mach-exynos/include/mach/cpufreq.h
@@ -14,6 +14,8 @@
  * This should be same with cpufreq_frequency_table
 */
 
+#define CPUFREQ_LEVEL_END	(L15 + 1)
+
 enum cpufreq_level_index {
 	L0, L1, L2, L3, L4,
 	L5, L6, L7, L8, L9,