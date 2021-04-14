574a96c209fa080d975aa6837cd7b4cfa5a03379
Yaroslav Zviezda <acroreiser@gmail.com>
2021-04-14 22:43:26 +0300

    HACK: signal/magisk: workaround: prevent sigkill to magiskd, just return 0 to userspace
    For something reasons Android 11 Go is too aggressive to magisk daemon and killing magiskd at init stage.
    Prevent this!
    
    To enable:
        echo 1 > /proc/sys/kernel/magisk_workaround

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2019,6 +2019,10 @@ extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 
+#ifdef CONFIG_ANDROID_DONT_KILL_MAGISK
+extern unsigned int sysctl_magisk_workaround;
+#endif
+
 enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_NONE,
 	SCHED_TUNABLESCALING_LOG,
