16ad64b0ec1df0f619b84a8dd98edb81fe9f5c5b
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-24 12:31:02 +0200

    module.c: fix module build through defining MODULE_SYMBOL_PREFIX.

--- kernel/module.c
+++ kernel/module.c
@@ -59,6 +59,13 @@
 #include <linux/pfn.h>
 #include <linux/bsearch.h>
 
+#ifdef CONFIG_SYMBOL_PREFIX
+#define MODULE_SYMBOL_PREFIX CONFIG_SYMBOL_PREFIX
+#else
+#define MODULE_SYMBOL_PREFIX ""
+#endif
+
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/module.h>
 
